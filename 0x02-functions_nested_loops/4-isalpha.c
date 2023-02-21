59ebc41945cc14c10cf2d1a177af9bc8962be596
Benita Emudianughe <emusbeny@gmail.com>
2023-02-21 09:20:56 -0800

    fixed trialing whitespace in task 4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * int _isalpha(int c) -  a function that checks for alphabetic character.
+ * _isalpha -  a function that checks for alphabetic character.
+ * @c: The character to be checked
  * Return: returns 1 if c is a letter and 0 if otherwise
  */
 int _isalpha(int c)
 {
-	if (c >= 65 && c < 122)
+	if (c >= 65 && c <= 122)
 		return (1);
-	else 
+	else
 		return (0);
-
 }
